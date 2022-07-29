a2a6fde0687ade4558b51aa87093aaccd0d79749
nghinguyenhuu <107188801+Nghinguyenhuu@users.noreply.github.com>
2022-07-30 01:47:41 +0700

    Home_View_More_Complete

--- delivery_food/lib/logged_in/screens/home.dart
+++ delivery_food/lib/logged_in/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 import '../../components/reusable_card.dart';
 import '../../components/reusable_home_card.dart';
@@ -8,8 +9,16 @@ import '../../constans/app_colors.dart';
 import '../../constans/app_stype.dart';
 import 'package:delivery_food/widget/pattern.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentindex = 0;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,7 +33,8 @@ class Home extends StatelessWidget {
                   children: [
                     const SizedBox(
                       width: 230,
-                      child: Text('Find Your Favorite Food', style: kTitleStyle),
+                      child:
+                          Text('Find Your Favorite Food', style: kTitleStyle),
                     ),
                     ReusableCard(
                       cardChild: IconButton(
@@ -76,165 +86,280 @@ class Home extends StatelessWidget {
                       width: 20,
                     ),
                     ReusableHomeCard(
-                        child: IconButton(
-                            onPressed: () {},
-                            icon: Image.asset('assets/icons/Filter.png')))
+                        child: Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Image.asset('assets/icons/Filter.png')),
+                    ))
                   ],
                 )),
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Container(
-                  height: 150,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.0),
-                      image:const DecorationImage(image: AssetImage('assets/images/Promocard.png'),fit: BoxFit.cover),
-                      gradient: const LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [
-                            AppColors.appLinerColorStart,
-                            AppColors.appLinerColorEnd
-                          ])),
-                  child:Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(width: 100,),
-                      Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const SizedBox(
-                              width: 144,
-                              height: 44,
-                              child: Text(
-                                'Special Deal For October',
-                                style: TextStyle(
-                                    color: Colors.white, fontFamily: 'BentonSans Bold',fontSize: 17),
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () {}, 
-                              child:Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: Colors.white
-                                ),
-                                child:Padding(
-                                  padding:const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                                  child: GradientText('Buy Now', colors:const [
-                                    AppColors.appLinerColorStart,
-                                    AppColors.appLinerColorEnd
-                                  ]),
-                                ))
-                              ),
-                          ],
-                        ),
-                    ],
-                  ),
-                  ),
-              ),
-              ),
-            
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Text(
-                      'Nearest Restaurant',
-                      style: kHomeSubjectStyle,
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: const Text(
-                        'View More',
-                        style: TextStyle(color: AppColors.orange),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            SliverGrid(
-                delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                  return ReusableCard(
-                      cardChild: SizedBox(
-                    height: 300,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset('assets/images/Logo.png'),
-                        const Text(
-                          'Vegan Resto',
-                          style: kHomeSubjectStyle,
-                        ),
-                        Text(
-                          '12 Mins',
-                          style: kHintInputStyle,
-                        )
-                      ],
-                    ),
-                  ));
-                }, childCount: 2),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.8,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20)),
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Text(
-                      'Popular Menu',
-                      style: kHomeSubjectStyle,
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: const Text(
-                        'View More',
-                        style: TextStyle(color: AppColors.orange),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            SliverList(
-              delegate:
-                  SliverChildBuilderDelegate((BuildContext context, int index) {
-                return ReusableCard(
-                    cardChild: ListTile(
-                  leading: Container(
-                    height: 64,
-                    width: 64,
-                    decoration: BoxDecoration(
-                        image: const DecorationImage(
-                            image: AssetImage('assets/images/MenuPhoto.png'),
-                            fit: BoxFit.cover),
-                        borderRadius: BorderRadius.circular(10.0)),
-                  ),
-                  title: const Text(
-                    'Herbal Pancake',
-                    style: kHomeSubjectStyle,
-                  ),
-                  subtitle: Text(
-                    'Warung Herbal',
-                    style: kHintInputStyle,
-                  ),
-                ));
-              }, childCount: 3),
-            )
+            currentindex == 0
+                ? buildHomeView()
+                : (currentindex == 1
+                    ? buildViewMoreRestaurant()
+                    : buildViewmoreMenu())
           ],
         ),
       ),
     );
+  }
+
+  buildViewmoreMenu() {
+    return MultiSliver(children: [
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Text(
+                'Popular Menu',
+                style: kHomeSubjectStyle,
+              ),
+            ],
+          ),
+        ),
+      ),
+      SliverList(
+        delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+          return ReusableCard(
+              cardChild: ListTile(
+            leading: Container(
+              height: 64,
+              width: 64,
+              decoration: BoxDecoration(
+                  image: const DecorationImage(
+                      image: AssetImage('assets/images/MenuPhoto.png'),
+                      fit: BoxFit.cover),
+                  borderRadius: BorderRadius.circular(10.0)),
+            ),
+            title: const Text(
+              'Herbal Pancake',
+              style: kHomeSubjectStyle,
+            ),
+            subtitle: Text(
+              'Warung Herbal',
+              style: kHintInputStyle,
+            ),
+          ));
+        }, childCount: 3),
+      )
+    ]);
+  }
+
+  buildViewMoreRestaurant() {
+    return MultiSliver(children: [
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Text(
+                'Nearest Restaurant',
+                style: kHomeSubjectStyle,
+              ),
+            ],
+          ),
+        ),
+      ),
+      SliverGrid(
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return ReusableCard(
+                cardChild: SizedBox(
+              height: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/Logo.png'),
+                  const Text(
+                    'Vegan Resto',
+                    style: kHomeSubjectStyle,
+                  ),
+                  Text(
+                    '12 Mins',
+                    style: kHintInputStyle,
+                  )
+                ],
+              ),
+            ));
+          }, childCount: 2),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.8,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20))
+    ]);
+  }
+
+  buildHomeView() {
+    return MultiSliver(children: [
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                image: const DecorationImage(
+                    image: AssetImage('assets/images/Promocard.png'),
+                    fit: BoxFit.cover),
+                gradient: const LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      AppColors.appLinerColorStart,
+                      AppColors.appLinerColorEnd
+                    ])),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 100,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      width: 144,
+                      height: 44,
+                      child: Text(
+                        'Special Deal For October',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'BentonSans Bold',
+                            fontSize: 17),
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Colors.white),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 10),
+                              child: GradientText('Buy Now', colors: const [
+                                AppColors.appLinerColorStart,
+                                AppColors.appLinerColorEnd
+                              ]),
+                            ))),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const Text(
+                'Nearest Restaurant',
+                style: kHomeSubjectStyle,
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    currentindex = 1;
+                  });
+                },
+                child: const Text(
+                  'View More',
+                  style: TextStyle(color: AppColors.orange),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      SliverGrid(
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return ReusableCard(
+                cardChild: SizedBox(
+              height: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/Logo.png'),
+                  const Text(
+                    'Vegan Resto',
+                    style: kHomeSubjectStyle,
+                  ),
+                  Text(
+                    '12 Mins',
+                    style: kHintInputStyle,
+                  )
+                ],
+              ),
+            ));
+          }, childCount: 2),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.8,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20)),
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const Text(
+                'Popular Menu',
+                style: kHomeSubjectStyle,
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    currentindex = 2;
+                  });
+                },
+                child: const Text(
+                  'View More',
+                  style: TextStyle(color: AppColors.orange),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      SliverList(
+        delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+          return ReusableCard(
+              cardChild: ListTile(
+            leading: Container(
+              height: 64,
+              width: 64,
+              decoration: BoxDecoration(
+                  image: const DecorationImage(
+                      image: AssetImage('assets/images/MenuPhoto.png'),
+                      fit: BoxFit.cover),
+                  borderRadius: BorderRadius.circular(10.0)),
+            ),
+            title: const Text(
+              'Herbal Pancake',
+              style: kHomeSubjectStyle,
+            ),
+            subtitle: Text(
+              'Warung Herbal',
+              style: kHintInputStyle,
+            ),
+          ));
+        }, childCount: 3),
+      )
+    ]);
   }
 }
